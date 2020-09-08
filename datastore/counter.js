b5a92edbea36ceee6a3314e9903bc733334909aa
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-08 14:09:58 -0700

    Pass all test but delete

--- datastore/counter.js
+++ datastore/counter.js
@@ -15,6 +15,10 @@ const zeroPaddedNumber = (num) => {
   return sprintf('%05d', num);
 };
 
+
+//take a callback as its argument
+//readFile() reads the contents of the file stored at the passed in path (as first argument), and then takes in a callback with error first and the data second, and then uses the data as the argument for the callback function passed in
+//im assuming in terms of error, that since the data is set to 0, that that would indicate that the fileData counter doesnt exist yet so the count is zero
 const readCounter = (callback) => {
   fs.readFile(exports.counterFile, (err, fileData) => {
     if (err) {
@@ -25,6 +29,11 @@ const readCounter = (callback) => {
   });
 };
 
+//take in a count and a call back
+//writes a counter string using the zeroPadded function and the count
+//then uses write file, using the exported file path here to 'counter.txt', take in the data (aka the counter string), and a callback that take in an error
+//if the error is truthy, it throws an error
+//else, it uses the callback in the write counter function on the counterString
 const writeCounter = (count, callback) => {
   var counterString = zeroPaddedNumber(count);
   fs.writeFile(exports.counterFile, counterString, (err) => {
@@ -38,9 +47,28 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+//need to save the current counter to a file in data
+//use the readCounter and WriteCounter functions
+//best to look at those functions to determine what they do
+exports.getNextUniqueId = (callback) => {
+  //im writing a counter.text file
+  //cant my callback just return the counter string?
+  //goal was to save the counter to the hardDrive. Right now it is saved to the hardDrive
+  //need to someone get the data that is a string to the returned to the nextUniqueId
+  //does a callback return to the function that called it
+  // so readcounter take a call back,(always error first and what I want to get out of this is the filedata, so that should be my second arugment)
+  readCounter((err, filedata) => {
+    //that callback function should invoke the write counter function that willt ake the fileData passed in from reading the file, and incrementing the count of it by one, since we are looking for a unique id
+    //write counter also take a call bacl that interacts with this data
+    //in write counter, the filedata + 1 is being converted into a counter string for our id tag of our to dos
+    //to get the unique id back to the create all that invoked get NextUniqueId, we need to use a callback that returns that data
+    //so again, error first, so first arg is an error, and the second arugument should be the idNumber for the new todo
+    writeCounter(filedata + 1, (err, id) => {
+      //now we need to return the idNumber using the callback from our original function call to
+      //again, error first, then data
+      callback(err, id);
+    });
+  });
 };
 
 
--- datastore/index.js
+++ datastore/index.js
@@ -7,36 +7,71 @@ var items = {};
 
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
+//right now, the counter is increasing through the memory using getNextUnique id
+//need to make getNextUniqueId read from a file that is stored in HD
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  //each toDo needs to be written in a file
+  //getNextUnique id, needs to return that number
+  //now we know that nextUniqueId takes a callback that will interact with out uniqueId
+  //so error first, then idNumber as the secondArugment
+  counter.getNextUniqueId((err, id) => {
+    //with this idNumber we want to create a new toDo file that is stored in the data directory with the id as the file path
+    //write a file, join the paths of the dataDir, and the idNumber as the name ofthe text fule
+    //the second arugmnet is the data that needs to be written - the learn says only store the to do text in the file
+    //third argumnet is a callback, error first, but after we write the file, we want to post the text to the UI
+    fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
+      callback(null, { id, text });
+    });
+  });
 };
 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+  //need to read over all the data files in the data directory
+  //readdir reacs the contents of a director,
+  //needs path as first argument, then cb with error first and the files of the directory as the second argument
+  //files here is an array of all the file names in the directoty
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (!files.length) {
+      callback(null, []);
+    } else {
+      var fileContents = _.map(files, function(file) {
+        return {id: file.slice(0, 5), text: file.slice(0, 5)};
+      });
+      //right now is returning an array of file names, but instead want an array of objects where each object is one file and each object has a key value pairs (id and text with value both being idNumber)
+      callback(null, fileContents);
+    }
   });
-  callback(null, data);
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  //need to find the file based on the id so we know readdir gives us a list of all the files with their name which is there id
+  //so if the id plus .txt is in the array,
+  //we read that file
+  //can probably skip reading through whole directory
+  // fs.readdir(exports.dataDir, (err, files) => {
+  fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, text) => {
+    if (!text) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback(null, {id: id, text: text.toString()});
+    }
+  });
 };
 
+
+
+
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  //retwrite the text stored in a toDO based on its id
+  fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, fileData) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err, text) => {
+        callback(null, { id, text });
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
